a948d473752a6883811c3b361b817d8755107bf3
Jae Chung <jae.chung99@gmail.com>
2018-07-16 16:47:43 +0800

    Update proposers.cpp

--- eosio.wps/proposers.cpp
+++ eosio.wps/proposers.cpp
@@ -68,6 +68,26 @@ class proposers : public eosio::contract {
 						 const string& website, const string& linkedin){
 			// authority of the user's account is required
 			require_auth(account);
+			
+			//verify that the inputs are not too short
+			eosio_assert(first_name.size() > 0, "first name should be more than 0 characters long");
+			eosio_assert(last_name.size() > 0, "last name should be more than 0 characters long");
+			eosio_assert(img_url.size() > 0, "not a valid image URL");
+			eosio_assert(bio.size() > 0, "bio should be more than 0 characters long");
+			eosio_assert(country.size() > 0, "country name should be more than 0 characters long");
+			eosio_assert(telegram.size() > 4, "not a valid Telegram username");
+			eosio_assert(website.size() > 0, "not a valid website URL");
+			eosio_assert(linkedin.size() > 0, "not a valid linkedin URL");
+
+			//verify that the inputs aren't too long
+			eosio_assert(first_name.size() < 128, "first name should be shorter than 128 characters.");
+			eosio_assert(last_name.size() < 128, "last name should be shorter than 128 characters.");
+			eosio_assert(img_url.size() < 128, "image URL should be shorter than 128 characters.");
+			eosio_assert(bio.size() < 256, "description should be shorter than 256 characters.");
+			eosio_assert(country.size() < 64, "country name should be shorter than 64 characters.");
+			eosio_assert(telegram.size() < 64, "telegram username should be shorter than 64 characters.");
+			eosio_assert(website.size() < 128, "website URL should be shorter than 128 characters.");
+			eosio_assert(linkedin.size() < 128, "linked URL should be shorter than 128 characters.");
 
 			proposer_index proposers(_self, _self);
 
