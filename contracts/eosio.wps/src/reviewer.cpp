529b249438bbdc03879c890e992d79571d33ca33
Jae Chung <jae.chung99@gmail.com>
2018-07-26 16:41:54 +0800

    Comment out assertion

--- contracts/eosio.wps/src/reviewer.cpp
+++ contracts/eosio.wps/src/reviewer.cpp
@@ -120,7 +120,7 @@ namespace eosiowps {
 		auto idx_index = proposals.get_index<N(idx)>();
 		auto itr_proposal = idx_index.find(proposal_id);
 		eosio_assert(itr_proposal != idx_index.end(), "Proposal not found in proposal table");
-		eosio_assert((*itr_proposal).status == proposal_status::PENDING, "Proposal::status is not proposal_status::PENDING");
+		// eosio_assert((*itr_proposal).status == proposal_status::PENDING, "Proposal::status is not proposal_status::PENDING");
 
 		idx_index.modify(itr_proposal, (*itr_proposal).owner, [&](auto& proposal){
 			proposal.status = proposal_status::REJECT;
