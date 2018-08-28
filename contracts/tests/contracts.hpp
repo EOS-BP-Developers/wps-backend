c911085a5d7d9da5ac05cd141e31021632e66369
Shenghua Su <shenghua.su@gmail.com>
2018-08-28 17:45:26 +0800

    build and test in docker

--- contracts/tests/contracts.hpp
+++ contracts/tests/contracts.hpp
@@ -4,16 +4,16 @@
 namespace eosio { namespace testing {
 
 struct contracts {
-   static std::vector<uint8_t> wps_wasm() { return read_wasm("/Users/yepp4you/Work/blockchain/wps-backend/contracts/eosio.wps/bin/eosio.wps/eosio.wps.wasm"); }
-   static std::string          wps_wast() { return read_wast("/Users/yepp4you/Work/blockchain/wps-backend/contracts/eosio.wps/bin/eosio.wps/eosio.wps.wast"); }
-   static std::vector<char>    wps_abi() { return read_abi("/Users/yepp4you/Work/blockchain/wps-backend/contracts/eosio.wps/bin/eosio.wps/eosio.wps.abi"); }
+   static std::vector<uint8_t> wps_wasm() { return read_wasm("/projects/wps-backend/contracts/tests/../eosio.wps/bin/eosio.wps/eosio.wps.wasm"); }
+   static std::string          wps_wast() { return read_wast("/projects/wps-backend/contracts/tests/../eosio.wps/bin/eosio.wps/eosio.wps.wast"); }
+   static std::vector<char>    wps_abi() { return read_abi("/projects/wps-backend/contracts/tests/../eosio.wps/bin/eosio.wps/eosio.wps.abi"); }
 
    struct util {
-      static std::vector<uint8_t> test_api_wasm() { return read_wasm("/Users/yepp4you/Work/blockchain/wps-backend/contracts/tests/test_contracts/test_api.wasm"); }
-      static std::vector<uint8_t> exchange_wasm() { return read_wasm("/Users/yepp4you/Work/blockchain/wps-backend/contracts/tests/test_contracts/exchange.wasm"); }
+      static std::vector<uint8_t> test_api_wasm() { return read_wasm("/projects/wps-backend/contracts/tests/test_contracts/test_api.wasm"); }
+      static std::vector<uint8_t> exchange_wasm() { return read_wasm("/projects/wps-backend/contracts/tests/test_contracts/exchange.wasm"); }
 
-      static std::vector<uint8_t> token_wasm() { return read_wasm("/Users/yepp4you/Work/blockchain/wps-backend/contracts/tests/test_contracts/eosio.token.wasm"); }
-      static std::vector<char> token_abi() { return read_abi("/Users/yepp4you/Work/blockchain/wps-backend/contracts/tests/test_contracts/eosio.token.abi"); }
+      static std::vector<uint8_t> token_wasm() { return read_wasm("/projects/wps-backend/contracts/tests/test_contracts/eosio.token.wasm"); }
+      static std::vector<char> token_abi() { return read_abi("/projects/wps-backend/contracts/tests/test_contracts/eosio.token.abi"); }
    };
 };
 }} //ns eosio::testing
